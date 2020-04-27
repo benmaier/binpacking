27dc643618c68c4d6d59f84d2e68f645f00fc142
Will Jones <will.jones@mscience.com>
2020-04-27 17:53:07 +0000

    Add key argument to imitate built-in max(), min() API

--- binpacking/tests/constant_bin_number.py
+++ binpacking/tests/constant_bin_number.py
@@ -5,3 +5,16 @@ def test_only_zero_weights():
     values = [0, 0, 0]
     bins = to_constant_bin_number(values, 4)
     assert bins == [[0, 0, 0], [], [], []]
+
+def test_key_func():
+    values = [
+        {'x': 'a', 'y': 1},
+        {'x': 'b', 'y': 5},
+        {'x': 'b', 'y': 3},
+    ]
+    bins = to_constant_bin_number(values, 2, key=lambda x: x['y'])
+
+    for bin_ in bins:
+        for item in bin_:
+            assert 'x' in item
+            assert 'y' in item
--- binpacking/tests/constant_volume.py
+++ binpacking/tests/constant_volume.py
@@ -5,3 +5,16 @@ def test_exact_fit():
     values = [1, 2, 1]
     bins = to_constant_volume(values, 2)
     assert len(bins) == 2
+
+def test_key_func():
+    values = [
+        {'x': 'a', 'y': 1},
+        {'x': 'b', 'y': 5},
+        {'x': 'b', 'y': 3},
+    ]
+    bins = to_constant_volume(values, 2, key=lambda x: x['y'])
+
+    for bin_ in bins:
+        for item in bin_:
+            assert 'x' in item
+            assert 'y' in item
