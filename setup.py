39dbdbabdecd6c1e83198f968c2af427737f3dc2
Ben F. Maier <benjaminfrankmaier@gmail.com>
2021-05-19 18:39:15 +0200

    forgot commas in list

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(name='binpacking',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7'
+                   'Programming Language :: Python :: 3.7',
                    'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9'
+                   'Programming Language :: Python :: 3.9',
                    ],
       zip_safe=False)
